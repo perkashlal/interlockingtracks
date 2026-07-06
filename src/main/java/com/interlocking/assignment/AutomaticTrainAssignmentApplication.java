c4a0d8d2462910be5d4fb5db7aa8b0c24219a7a2
Perkash Lal <parkakhlal450@gmail.com>
2026-07-06 02:13:10 +0200

    Add Spring Boot application entry point

---
+++ src/main/java/com/interlocking/assignment/AutomaticTrainAssignmentApplication.java
@@ -0,0 +1,13 @@
+package com.interlocking.assignment;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutomaticTrainAssignmentApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AutomaticTrainAssignmentApplication.class, args);
+    }
+}
+
